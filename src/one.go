13b8c2872f7991ed4712f656750d2a39ba061dae
kkh061101 <kkh061101@naver.com>
2023-03-15 16:33:47 +0900

    Update one.go

--- src/one.go
+++ src/one.go
@@ -1,72 +1,25 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
-// 백준 1021번
 func main() {
-	var max int //최대 크기
-	var n int   // 횟수
-	fmt.Scan(&max, &n)
-	p := 1 // 위치 정보
-	var arr []int
+	a := 0
+	b := 0
 	sum := 0
-	distance := 0
-	rotation := 0
-	for i := 0; i < n; i++ {
-		var num int
-		fmt.Scan(&num)
-		if p == num { //동일할 때
-			if !contains(arr, p+1) {
-				sum++
-			}
-			p = p%max + 1
-		} else if (num-p+max)%max > (p-num+max)%max { // 왼쪽이 더 가까울때
-			distance = (p - num + max) % max
-			for j := 0; j < distance; j++ {
-				if !contains(arr, p-1) {
-					sum++
-				}
-				p = (p + max - 1) % max
-			}
-			p = p%max + 1
-		} else if (num-p+max)%max < (p-num+max)%max { // 오른쪽이 더 가까울때
-			distance = (num - p + max) % max
-			for j := 0; j < distance; j++ {
-				if !contains(arr, p+1) {
-					sum++
-				}
-
-			}
-		} else { // 거리가 동일 할때
-			rotation = 2
-			distance = (num - p + max) % max
-		}
-
-		for j := 0; j < distance; j++ {
-			switch rotation {
-			case 0:
-				p = (num)%max + 1
-				if !contains(arr, p+1) {
-					sum++
-				}
-			case 1:
-				if !contains(arr, p+1) {
-
-				}
-			}
-			arr = append(arr, num)
-
-		}
+	fmt.Scan(&a, &b)
+	osum := a * b
+	for i := 0; i < 3; i++ {
+		sum = a * (b % times(10, i+1)) / times(10, i)
+		println(sum)
+		b = b - b%times(10, i+1)
 	}
+	println(osum)
 }
 
-func contains(elems []int, v int) bool {
-	for _, s := range elems {
-		if v == s {
-			return true
-		}
+func times(n int, k int) int {
+	sum := 1
+	for i := 0; i < k; i++ {
+		sum *= n
 	}
-	return false
+	return sum
 }
