34af35085f029e804ba48129bdd36296b2d8e7a4
kkh061101 <kkh061101@naver.com>
2023-12-16 18:53:47 +0900

    it works slowly

--- src/B15651.go
+++ src/B15651.go
@@ -1,42 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
+var reader *bufio.Reader = bufio.NewReader(os.Stdin)
+var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
+
 var n, m int
-var list [8]int
-var visit [8]bool
+var list [7]int
+var visit [7]bool
 
 func main() {
-	fmt.Scan(&n, &m)
+	fmt.Fscan(reader, &n, &m)
 	recursion(0)
 }
 
 func recursion(sidx int) {
+	defer writer.Flush()
 	if sidx == m {
-		// var check bool
-		// for i := 0; i < m-1; i++ {
-		// 	if list[i] > list[i+1] {
-		// 		check = true
-		// 	}
-		// }
-		// if check {
-		// 	return
-		// }
 		for i := 0; i < m; i++ {
-			fmt.Printf("%d ", list[i]+1)
+			fmt.Fprintf(writer, "%d ", list[i]+1)
 		}
-		fmt.Println("")
+		fmt.Fprintf(writer, "\n")
 		return
 	}
 	for i := 0; i < n; i++ {
 		if !visit[i] {
-			visit[i] = true
+			// visit[i] = true
 			list[sidx] = i
-			recursion(sidx)
-			// recursion(sidx + 1)
-			visit[i] = false
+			// recursion(sidx)
+			recursion(sidx + 1)
+			// visit[i] = false
 		}
 	}
 }
