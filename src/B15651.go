4440c0d1eaf781a3ef94e46540881e9ffcc0906f
kkh061101 <kkh061101@naver.com>
2023-12-22 21:03:35 +0900

    backup

--- src/B15651.go
+++ src/B15651.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"sort"
 )
 
 var reader *bufio.Reader = bufio.NewReader(os.Stdin)
@@ -12,28 +13,48 @@ var writer *bufio.Writer = bufio.NewWriter(os.Stdout)
 var n, m int
 var list [7]int
 var visit [7]bool
+var newlist [7]int
+
+var lagacylist [100000][7]int
+
+var location int = 0
+
+var temp [7]int
 
 func main() {
+	defer writer.Flush()
 	fmt.Fscan(reader, &n, &m)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(reader, &newlist[i])
+	}
+	sort.Ints(newlist[0:n])
 	recursion(0)
 }
 
 func recursion(sidx int) {
-	defer writer.Flush()
 	if sidx == m {
 		for i := 0; i < m; i++ {
-			fmt.Fprintf(writer, "%d ", list[i]+1)
+			temp[i] = newlist[list[i]]
 		}
+		for i := location; i >= 0; i-- {
+			if lagacylist[i] == temp {
+				return
+			}
+		}
+		for i := 0; i < m; i++ {
+			fmt.Fprintf(writer, "%d ", temp[i])
+		}
+		lagacylist[location] = temp
+		location++
 		fmt.Fprintf(writer, "\n")
 		return
 	}
 	for i := 0; i < n; i++ {
-		if !visit[i] {
-			// visit[i] = true
-			list[sidx] = i
-			// recursion(sidx)
-			recursion(sidx + 1)
-			// visit[i] = false
-		}
+		// if !visit[i] {
+		// 	visit[i] = true
+		list[sidx] = i
+		recursion(sidx + 1)
+		// 	visit[i] = false
+		// }
 	}
 }
